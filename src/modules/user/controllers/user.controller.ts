e29350e06b1695ce98a428915348b73d1c7dc084
Yauheni Udavihin <e.udavihin@itransition.com>
2022-07-16 08:41:41 +0300

    Implement find user by id

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -4,14 +4,15 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Param,
   Post,
 } from '@nestjs/common';
 import { UserRepository } from '../services/user.repository.js';
-import { User } from '../types/user.interface.js';
 import { CreateUserDto } from '../dtos/createUser.dto.js';
 import { DuplicateEntryError } from '../../../errors/duplicateEntry.error.js';
 import { ValidationError } from '../../../errors/validation.error.js';
 import { UserResponseDto } from '../dtos/user.responseDto.js';
+import { NotFoundError } from 'rxjs';
 
 @Controller('user')
 export class UserController {
@@ -33,6 +34,24 @@ export class UserController {
         error instanceof ValidationError
       ) {
         throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+      } else {
+        throw new HttpException(
+          error.message,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  @Get(':id')
+  async getOneById(@Param('id') id: string): Promise<UserResponseDto> {
+    try {
+      return this.userRepository.findOneById(id);
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+      } else if (error instanceof NotFoundError) {
+        throw new HttpException(error.message, HttpStatus.NOT_FOUND);
       } else {
         throw new HttpException(
           error.message,
--- src/modules/user/services/user.repository.ts
+++ src/modules/user/services/user.repository.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '../types/user.interface.js';
 import { CreateUserDto } from '../dtos/createUser.dto.js';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4, validate } from 'uuid';
 import * as bcrypt from 'bcrypt';
 import { UserValidator } from './user.validator.js';
 import { ValidationError } from '../../../errors/validation.error.js';
 import { UserResponseDto } from '../dtos/user.responseDto.js';
+import { NotFoundError } from 'rxjs';
 
 let users: User[] = [];
 
@@ -39,6 +40,17 @@ export class UserRepository {
     };
 
     users.push(user);
+    return new UserResponseDto(user);
+  }
+
+  findOneById(id: string): UserResponseDto {
+    if (!validate(id)) {
+      throw new ValidationError(`Provided id: ${id} is not valid`, id);
+    }
+    const user: User = users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundError(`User with id: ${id} not found`);
+    }
     return new UserResponseDto(user);
   }
 }
